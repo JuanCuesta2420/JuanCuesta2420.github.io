a806570f3e669ab46191115bcf9b10b3ae4fc3e6
Juan Cuesta <cuesta.j@northeastern.edu>
2024-01-26 18:17:36 -0500

    Changes in index and java file

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,28 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/Cathedral-in-Quito-Ecuador.png");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Quito is the best place on earth, ${myName}`;
+    }
+  }
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Quito is the best place on earth, ${storedName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+
